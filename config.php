5137dffecca5fada87d9207bd7662ee44c2d2620
Silva <silva.haberl1989@gmail.com>
2016-04-01 17:43:11 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connect = mysql_connect("localhost","silvahab","s1be3Rh6P2") or die(mysql_error());
+$connect = mysql_connect("localhost","silvahab","") or die(mysql_error());
 mysql_select_db("silvahab_weather123");
  
 
